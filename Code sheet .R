16498e3a699803571d73ea0a39d75f3cbcacfe14
bagarrett <brighton_garrett1@baylor.edu>
2021-05-02 14:00:23 -0700

    started writing correlation code

--- Code sheet .R
+++ Code sheet .R
@@ -1,7 +1,25 @@
 data <- read.csv("Mines_Park_pH_Fault.csv", stringsAsFactors = FALSE)
 attr(data$dateTime, "tzone") <- "America/Denver"
 
+library(lubridate)
 
+
+#finding where the fault occurred and what row the observation is at
+colnames(data)
+dim(data)
+head(data$dateTime)
+tail(data$dateTime)
+
+month.val <- month(data$dateTime)
+day.val <- day(data$dateTime)
+hour.val <- hour(data$dateTime)
+fault.time <- which(month.val==4 & day.val==24 & hour.val==10)[1]
+data$dateTime[fault.time]
+
+#narrowing the data to just before the fault occurred
+data_before_fault <- data[0:fault.time , ]
+
+#narrowing the data to just the prefered columns
 data_edited <- data[, c("bio_1_blow_flow", 
                         "bio_2_blow_flow", 
                         "mbr_1_tmp", 
@@ -19,26 +37,33 @@ data_edited <- data[, c("bio_1_blow_flow",
                         "mbr_2_mode_4", 
                         "ras_ph")]
 
-data_firsthalf <- data[, c("bio_1_blow_flow", 
+data_firstquarter <- data_before_fault[, c("bio_1_blow_flow", 
                            "bio_2_blow_flow", 
                            "mbr_1_tmp", 
                            "mbr_2_tmp",
-                           "ambient_temp", 
-                           "bio_1_phase_1", 
-                           "bio_1_phase_2", 
                            "ras_ph")]
-data_secondhalf <- data[, c("bio_2_phase_1",
+                           
+data_secondquarter <- data_before_fault[,c ("ambient_temp", 
+                              "bio_1_phase_1", 
+                              "bio_1_phase_2", 
+                              "ras_ph")]
+
+data_thirdquarter <- data_before_fault[, c("bio_2_phase_1",
                            "bio_2_phase_2", 
                            "mbr_1_mode_1", 
-                           "mbr_1_mode_2", 
-                           "mbr_1_mode_4",
-                           "mbr_2_mode_1", 
-                           "mbr_2_mode_2", 
-                           "mbr_2_mode_4", 
+                           "mbr_1_mode_2",
                            "ras_ph")]
+data_fourthquarter <- data_before_fault[, c("mbr_1_mode_4",
+                               "mbr_2_mode_1", 
+                               "mbr_2_mode_2", 
+                               "mbr_2_mode_4", 
+                               "ras_ph")]
 
-pairs(ras_ph ~ ., data = data_firsthalf)
-pairs(ras_ph ~ ., data = data_secondhalf)
+#seeing which variables are correlated with the other variables
+pairs(ras_ph ~ ., data = data_firstquarter)
+pairs(ras_ph ~ ., data = data_secondquarter)
+pairs(ras_ph ~ ., data = data_thirdquarter)
+pairs(ras_ph ~ ., data = data_fourthquarter)
 
 
 
